14a3cab4d1beaeca881292575cdb89981954ede7
ogzburakbsrn <basaranoguzburak@gmail.com>
2022-05-26 11:24:25 +0300

    refactored file

--- group_pji_final/group_pji.py
+++ group_pji_final/group_pji.py
@@ -1,17 +1,19 @@
-import os, shutil, sys
+import os
+import shutil
+import sys
 
-#.exe --> sys.executable, .py --> __file__
+# .exe --> sys.executable, .py --> __file__
 try:
-   if sys.frozen or sys.importers:
-      script_directory = os.path.dirname(sys.executable)
+    if sys.frozen or sys.importers:
+        script_directory = os.path.dirname(sys.executable)
 except AttributeError:
-   script_directory = os.path.dirname(os.path.realpath(__file__))
+    script_directory = os.path.dirname(os.path.realpath(__file__))
 
-#creates a backup folder if it doesn't exist
+# creates a backup folder if it doesn't exist
 if not os.path.isdir(os.path.join(script_directory, 'Backup')):
     os.mkdir(os.path.join(script_directory, 'Backup'))
 
-#moves all files to backup folder for if anything goes wrong
+# moves all files to backup folder for if anything goes wrong
 with os.scandir(script_directory) as it:
     for entry in it:
         if entry.name.endswith('html') and entry.is_file():
@@ -20,14 +22,14 @@ with os.scandir(script_directory) as it:
             except:
                 print("An exception occurred")
 
-#goes through all files and moves them to pji folder if folder exists, if file with same name already exists it removes
-#the source file
-#creates new directory if pji folder isn't found, then moves the files in it
+# goes through all files and moves them to pji folder if folder exists, if file with same name already exists it removes
+# the source file
+# creates new directory if pji folder isn't found, then moves the files in it
 with os.scandir(script_directory) as it:
     for entry in it:
         if entry.name.endswith('html') and entry.is_file():
-            htmlPji = entry.name[0:7]   #pji           
-            htmlPath = entry.path       #path pji
+            htmlPji = entry.name[0:7]  # pji
+            htmlPath = entry.path  # path pji
             if os.path.isdir(os.path.join(script_directory, htmlPji)):
                 try:
                     shutil.move(htmlPath, os.path.join(script_directory, htmlPji))
@@ -38,4 +40,3 @@ with os.scandir(script_directory) as it:
                 shutil.move(htmlPath, os.path.join(script_directory, htmlPji))
             else:
                 print("Something wrong happened while moving files")
-                
